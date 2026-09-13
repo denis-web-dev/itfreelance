321502d514c14c12d9cc80f6dd0053eb61e0ee01
denis-web-dev <denis13.2011@mail.ru>
2026-09-13 18:18:54 +0300

    исправление: добавлен basename для React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/itfreelance">
       <AuthProvider>
         <App />
       </AuthProvider>
