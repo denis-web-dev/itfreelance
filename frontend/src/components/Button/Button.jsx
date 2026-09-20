5c336d5caaf1776b8c9e4201de754eda8d990aae
Denis-KotelnikovV <den_code@mail.ru>
2026-09-20 23:23:06 +0300

    Настроил конфиг prettier.json

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -2,30 +2,24 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
-function Button({
-  variant = 'default',
-  children,
-  ariaLabel,
-  onClick,
-  className,
-}) {
-  return (
-    <button
-      className={clsx(styles.button, styles[variant], className)}
-      aria-label={ariaLabel}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+function Button({ variant = 'default', children, ariaLabel, onClick, className }) {
+	return (
+		<button
+			className={clsx(styles.button, styles[variant], className)}
+			aria-label={ariaLabel}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
 }
 
 Button.propTypes = {
-  variant: PropTypes.string,
-  children: PropTypes.node.isRequired,
-  ariaLabel: PropTypes.string,
-  onClick: PropTypes.func,
-  className: PropTypes.string,
+	variant: PropTypes.string,
+	children: PropTypes.node.isRequired,
+	ariaLabel: PropTypes.string,
+	onClick: PropTypes.func,
+	className: PropTypes.string,
 };
 
 export default Button;
